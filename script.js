098ac0ad45f72a3633a88424904f89f3f7519a99
fawwazaditya <fawwazaditya25@gmail.com>
2024-09-10 13:26:42 +0700

    menambahkan file

---
+++ script.js
@@ -0,0 +1,34 @@
+const pilihan = document.querySelector(".pilihan");
+const container = document.querySelector(".container");
+const tidak = document.querySelector(".tidakmau");
+const inputElement = document.getElementById("angka");
+const submitButton = document.getElementById("submitBtn");
+
+// console.log(inputElement);
+function komputer() {
+  return Math.round(Math.random() * 9) + 1; // Menghasilkan angka 1-10
+}
+// console.log(komputer());
+let kesempatan = 0;
+
+const milih = pilihan.addEventListener("click", function (e) {
+  const pi = e.target.className;
+  if (pi == "IYA") {
+    pilihan.classList.toggle("none");
+    container.classList.toggle("none");
+    const angka = komputer();
+    submitButton.addEventListener("click", function () {
+      const inputValue = parseInt(inputElement.value);
+      if (inputValue > angka) {
+        alert("nilai yang anda masukan lebih besar  dari komputer");
+      } else if (inputValue < angka) {
+        alert("nilai yang anda masukan lebih kecil  dari komputer");
+      } else if (inputValue === angka) {
+        alert("SELAMAT ANDA MENANG");
+      }
+    });
+  } else if (pi == "tidak") {
+    pilihan.classList.toggle("none");
+    tidak.classList.toggle("none");
+  }
+});
