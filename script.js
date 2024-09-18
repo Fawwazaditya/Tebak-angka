fe7cd637e9986537e9b4ccf499c093dbe0881e60
fawwazaditya <fawwazaditya25@gmail.com>
2024-09-18 22:32:54 +0700

    done

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const container = document.querySelector(".container");
 const tidak = document.querySelector(".tidakmau");
 const inputElement = document.getElementById("angka");
 const submitButton = document.getElementById("submitBtn");
-const kalah = document.querySelector(".gagal H3.jumlah");
+const kalah = document.querySelectorAll(".gagal H3.kesempatan");
 
 function komputer() {
   return Math.round(Math.random() * 9) + 1; // Menghasilkan angka 1-10
 }
 
-let kesempatan = 0;
+let kesempatan = 5;
 let angkaKomputer; // Simpan angka acak komputer di luar event listener
 
 // Event listener untuk pilihan IYA/TIDAK
@@ -20,7 +20,7 @@ pilihan.addEventListener("click", function (e) {
     container.classList.toggle("none");
 
     angkaKomputer = komputer(); // Dapatkan angka komputer saat game dimulai
-    kesempatan = 0; // Reset kesempatan setiap kali game dimulai
+    kesempatan = 5; // Reset kesempatan setiap kali game dimulai
   } else if (pi == "tidak") {
     pilihan.classList.toggle("none");
     tidak.classList.toggle("none");
@@ -30,9 +30,13 @@ pilihan.addEventListener("click", function (e) {
 // Event listener untuk tombol submit
 submitButton.addEventListener("click", function () {
   const inputValue = parseInt(inputElement.value);
-
+  if (Number.isNaN(inputValue)) {
+    alert("Masukkan hanya angka!");
+    inputElement.value = "";
+    return;
+  }
   // Jika kesempatan masih kurang dari 5
-  if (kesempatan < 5) {
+  if (kesempatan >= 0) {
     if (inputValue > angkaKomputer) {
       alert("Nilai yang anda masukkan lebih besar dari komputer");
     } else if (inputValue < angkaKomputer) {
@@ -42,12 +46,13 @@ submitButton.addEventListener("click", function () {
       window.close;
       return; // Berhenti jika pemain menang
     }
-    kesempatan++; // Tambahkan kesempatan
+    kesempatan--;
+    kalah.innerHTML = kesempatan;
     inputElement.value = "";
   }
 
   // Jika kesempatan habis
-  if (kesempatan >= 5) {
+  if (kesempatan <= 0) {
     alert("ANDA KALAH, COBA LAIN KALI");
     window.close();
   }
